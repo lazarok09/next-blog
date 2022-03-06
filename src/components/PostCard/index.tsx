6b72cda676ce5cf026161dd1d7586cd8f569d6a9
lazarok09 <lazarok09@gmail.com>
2022-03-06 10:56:06 -0300

    feat(<PostCard />): component e teste

---
+++ src/components/PostCard/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { StrapiImage } from '../../shared-types/StrapiImage';
+import { Heading } from '../Heading';
+import * as Styled from './styles';
+
+export type PostCardProps = {
+  title: string;
+  cover: StrapiImage;
+  excerpt: string;
+  slug: string;
+};
+export const PostCard = ({ title, cover, excerpt, slug }: PostCardProps) => {
+  return (
+    <Styled.Wrapper>
+      <Link href={`/post/${slug}`}>
+        <a>
+          <Styled.Cover src={cover.url} alt={title} />
+        </a>
+      </Link>
+
+      <Heading as="h2" size="small">
+        <Link href={`/post/${slug}`}>
+          <a>{title} </a>
+        </Link>
+      </Heading>
+
+      <Styled.Excerpt>{excerpt}</Styled.Excerpt>
+    </Styled.Wrapper>
+  );
+};
+
+export default PostCard;
---
+++ src/components/PostCard/styles.ts
@@ -0,0 +1,38 @@
+import styled, { css } from 'styled-components';
+import { Title as HeadingStyles } from '../Heading/styles';
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    ${HeadingStyles} {
+      margin: 0;
+      margin-top: calc(${theme.spacings.small} - 0.6rem);
+      margin-bottom: ${theme.spacings.small};
+    }
+
+    & a {
+      text-decoration: none;
+      color: inherit;
+      transition: all 300ms ease-in-out;
+    }
+    & a:hover {
+      color: ${theme.colors.secondary};
+    }
+    // when we hover this element & i want to change the behavier of the a tag
+    &:hover a {
+      color: ${theme.colors.secondary};
+    }
+    &:hover img {
+      opacity: 0.8;
+    }
+  `}
+`;
+export const Cover = styled.img`
+  ${() => css`
+    max-width: 100%;
+    transition: opacity 300ms ease-in-out;
+  `}
+`;
+export const Excerpt = styled.p`
+  ${() => css`
+    word-break: break-word;
+  `}
+`;
