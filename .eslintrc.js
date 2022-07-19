024cce89028b4bd73049e58bd25fb5f2efa4f954
lazarok09 <lazaro.souza@agilecontent.com>
2022-07-18 21:17:47 -0300

    chore(eslint): resolve interferencia do eslint com next

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'next/core-web-vitals',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
