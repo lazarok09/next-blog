98222d7069f0f97a7856b104f7d9bdc10abaab62
lazarok09 <lazarok09@gmail.com>
2022-10-20 22:35:10 -0300

    fix: fix layout mobile padding

--- src/components/ArticleHeader/styles.ts
+++ src/components/ArticleHeader/styles.ts
@@ -10,6 +10,9 @@ export const Wrapper = styled.header`
     ${HeadingStyles} {
       margin: 0;
       margin-bottom: ${theme.spacings.medium};
+      @media ${theme.media.lteMedium} {
+        font-size: calc(${theme.font.sizes.medium} + 1rem);
+      }
     }
   `}
 `;
@@ -17,6 +20,9 @@ export const Excerpt = styled.p`
   ${({ theme }) => css`
     margin: ${theme.spacings.medium} 0;
     font-size: ${theme.font.sizes.medium};
+    @media ${theme.media.lteMedium} {
+      font-size: calc(${theme.font.sizes.small} + 0.2rem);
+    }
   `}
 `;
 export const Cover = styled.img`
