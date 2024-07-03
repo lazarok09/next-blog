b4c93720d39d3ee10a067c968aacb0daa4486def
lazarok09 <lazarok09@gmail.com>
2024-07-03 19:29:07 -0300

    feat: fix bson mapping

--- backend/models/authors.go
+++ backend/models/authors.go
@@ -3,12 +3,12 @@ package authors
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Author struct {
-	Id           primitive.ObjectID `bson:"_id"`
-	DisplayName  string             `bson:"displayName"`
-	Slug         string             `bson:"slug"`
-	Published_At primitive.DateTime `bson:"published_at"`
-	Created_At   primitive.DateTime `bson:"created_at"`
-	Updated_At   primitive.DateTime `bson:"updated_at"`
-	CreatedBy    string             `bson:"createdBy"`
-	UpdateBy     string             `bson:"updatedBy"`
+	Id          primitive.ObjectID `bson:"_id"`
+	DisplayName string             `bson:"displayName"`
+	Slug        string             `bson:"slug"`
+	PublishedAt primitive.DateTime `bson:"published_at"`
+	CreatedAt   primitive.DateTime `bson:"createdAt"`
+	UpdatedAt   primitive.DateTime `bson:"updatedAt"`
+	CreatedBy   string             `bson:"created_by"`
+	UpdateBy    string             `bson:"updated_by"`
 }
