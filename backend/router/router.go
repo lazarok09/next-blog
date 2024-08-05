b13efae186b08f33debd87e8b40b6b09aa3467b9
lazarok09 <lazarok09@gmail.com>
2024-08-05 15:40:07 -0300

    feat: add cors to localhost

--- backend/router/router.go
+++ backend/router/router.go
@@ -14,27 +14,46 @@ import (
 	"github.com/lazarok09/go-blog/controllers/upload_file"
 )
 
+const AllowedHost = "http://localhost:3000"
+
+func insertCors(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", AllowedHost)
+}
+
 func InitRoutes() {
 
 	http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+		insertCors(w)
 		posts.Handler(w, r)
 	})
 	http.HandleFunc("/settings", func(w http.ResponseWriter, r *http.Request) {
+		insertCors(w)
+
 		settings.Handler(w, r)
 	})
 	http.HandleFunc("/authors", func(w http.ResponseWriter, r *http.Request) {
+		insertCors(w)
+
 		authors.Handler(w, r)
 	})
 	http.HandleFunc("/categories", func(w http.ResponseWriter, r *http.Request) {
+		insertCors(w)
+
 		categories.Handler(w, r)
 	})
 	http.HandleFunc("/tags", func(w http.ResponseWriter, r *http.Request) {
+		insertCors(w)
+
 		tags.Handler(w, r)
 	})
 	http.HandleFunc("/components_menu_menu_links", func(w http.ResponseWriter, r *http.Request) {
+		insertCors(w)
+
 		components_menu_menu_links.Handler(w, r)
 	})
 	http.HandleFunc("/upload_file", func(w http.ResponseWriter, r *http.Request) {
+		insertCors(w)
+
 		upload_file.Handler(w, r)
 	})
 
